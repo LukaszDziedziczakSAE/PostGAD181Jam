4d1d6b65e4f7613dba76e6752a156b2704ae53d0
Lukasz Dziedziczak <wukash@gmail.com>
2024-05-01 19:12:16 +0930

    Controller FIxes

--- Assets/Scripts/UI/UI_PauseMenu.cs
+++ Assets/Scripts/UI/UI_PauseMenu.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_PauseMenu : MonoBehaviour
 {
+    [SerializeField] EventSystem eventSystem;
+    [SerializeField] Button resetButton;
+
+    private void OnEnable()
+    {
+        eventSystem.firstSelectedGameObject = resetButton.gameObject;
+    }
 
     public void OnResetPress()
     {
